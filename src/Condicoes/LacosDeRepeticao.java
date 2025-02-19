6203da5dad8f313b2951f1893fc9a82c41f1f4c8
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-19 17:21:27 -0300

    Loops

---
+++ src/Condicoes/LacosDeRepeticao.java
@@ -0,0 +1,17 @@
+package Condicoes;
+
+public class LacosDeRepeticao {
+    public static void main(String[] args) {
+        int numeroDeClones = 1;
+        int numeroMaximoDeClones = 40;
+
+//        while (numeroDeClones <= numeroMaximoDeClones) {
+//            System.out.println(String.format("O Naruto fez %d clone%s das sombras", numeroDeClones, numeroDeClones == 1 ? "" : "s"));
+//            numeroDeClones++;
+//        }
+
+
+        for (int i = 1; i <= 40; i++) {
+            System.out.printf("O Naruto fez %d clone%s das sombras%n", i, i == 1 ? "" : "s");
+        }
+}}
