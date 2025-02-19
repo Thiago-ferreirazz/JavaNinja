a9f67197fd2ca7f3183589fb2b7b57d75a20ca9c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-19 17:52:09 -0300

    Array

---
+++ src/Condicoes/Array.java
@@ -0,0 +1,36 @@
+package Condicoes;
+
+public class Array {
+    public static void main(String[] args) {
+        String[] ninjas = new String[4];
+        ninjas[0] = "Naruto Uzumaki";
+        ninjas[1] = "Sakura Haruno";
+        ninjas[2] = "Sasuke Uchiha";
+        ninjas[3] = "Hinata Hyuga";
+
+        System.out.println(ninjas[1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
