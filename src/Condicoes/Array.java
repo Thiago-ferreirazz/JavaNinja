60a15decf8e706e43784b81d138f4f739aca95fb
Thiago Fereira <labsfiap@fiap.com.br>
2025-03-20 17:25:47 -0300

    2D Array

--- src/Condicoes/Array.java
+++ src/Condicoes/Array.java
@@ -11,26 +11,25 @@ public class Array {
         for (int i = 0; i < ninjas.length; i++) {
             System.out.println(ninjas[i] + "é o " + (i+1)+ "° ninja");
         }
+        //Two dimensional Array
+        String [][] ninjasEAldeias = new String[3][3];
 
+        ninjasEAldeias[0][0] = "Konoha";
+        ninjasEAldeias[0][1] = "Naruto Uzumaki";
+        ninjasEAldeias[0][2] = "Sasuke Uchiha";
 
+        ninjasEAldeias[1][0] = "Nevoa";
+        ninjasEAldeias[1][1] = "Zabuza";
+        ninjasEAldeias[1][2] = "Haku";
 
+        ninjasEAldeias[2][0] = "Deserto";
+        ninjasEAldeias[2][1] = "Gaara";
+        ninjasEAldeias[2][2] = "Temari";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        for (int y = 0; y < ninjasEAldeias.length; y++) {
+            for (int i = 0; i < ninjasEAldeias.length; i++) {
+                System.out.println("Aldeia " + ninjasEAldeias[y][i]);
+            }
+        }
     }
 }
